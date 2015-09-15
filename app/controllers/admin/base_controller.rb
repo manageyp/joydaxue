3ec4f9873acff598709d86968716585e6eb5909c
Henry Lee <manageyp@gmail.com>
2015-09-15 15:02:59 +0800

    [Change] authenticate user login and permission

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -11,11 +11,28 @@ module Admin
     end
 
     def authentication
-      if session[:user_name].present?
-        @user_name = session[:user_name]
+      if session[:username].present?
+        @username = session[:username]
+        @user = User.where(name: session[:username]).first
+        if !@user.is_active?
+          flash[:error] = "您的账号被禁用，请联系管理员。"
+          redirect_to root_path and return
+        elsif super_admin? ||
+          Permission.permitted?(@user, controller_name, action_name)
+          return true
+        else
+          render template: "admin/common/unauthorized"
+          return false
+        end
       else
-        redirect_to root_path
+        flash[:error] = "请您先登录。"
+        redirect_to root_path and return
       end
     end
+
+    def super_admin?
+      ['henrylee'].include?(session[:username])
+    end
+
   end
 end
